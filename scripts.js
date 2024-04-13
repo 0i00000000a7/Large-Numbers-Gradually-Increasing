4915d6b0b265743e24282010048d91e4a28d06fb
0i00000000a7 <149916146+0i00000000a7@users.noreply.github.com>
2024-04-13 11:49:33 +0800

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,31 +2,36 @@ time = localStorage.getItem('LNGI') !== undefined ? Number(localStorage.getItem(
 test = 0
 endScreenShowed = false
 isShowingMilestone = false
-milestones = [0,8525,13166,16627,22450,33938,52391,80131,89348,130069,133649,185817,229228,233834,288198]
-milestoneDisplays = ['0','10,000,000,000','10<sup>100</sup>','10<sup>1,000,000</sup>','10<sup>10<sup>100</sup></sup>','10<sup>10<sup>10<sup>10,000,000,000</sup></sup></sup>','10^^10','10^^10^^10^^10','10^^^1,000,000','10^^^^5','10{10}10','10{1,000,000}10','10{10{10{10{10}10}10}10}10','10{{1}}1,000','10{{2}}5']
+milestones = [0, 8525, 13166, 16627, 22450, 33938, 52391, 80131, 89348, 130069, 133649, 185817, 229228, 233834, 288198, 1033200, 1857554]
 Infinity_symbol = '<div class="rotate-90">8</div>'
-totalTime = 288198
+milestoneDisplays = ['0', '10,000,000,000', '10<sup>100</sup>', '10<sup>1,000,000</sup>', '10<sup>10<sup>100</sup></sup>', '10<sup>10<sup>10<sup>10,000,000,000</sup></sup></sup>', '10^^10', '10^^10^^10^^10', '10^^^1,000,000', '10^^^^5', '10{10}10', '10{1,000,000}10', '10{10{10{10{10}10}10}10}10', '10{{1}}1,000', '10{{2}}5', '10{{3}}6', '10{{1,000,000}}10']
+Infinity_symbol = '<div class="rotate-90">8</div>'
+totalTime = 1857554
 
 function update() {
-  time++
-  if (time>=288198) time = 288198
-  localStorage.setItem('LNGI',time)
+  if (time >= 288198) time += ((Math.log10(time+1)+1)*4)
+  else time++
+  if (time >= 1857554) time = 1857554
+  if (time >= Number.MAX_VALUE) time = Number.MAX_VALUE
+  localStorage.setItem('LNGI', time)
   document.getElementById("number").innerHTML = timeToNumber(time)
   document.getElementById("milestones").innerHTML = getMilestoneText()
   document.getElementById("popup").innerHTML = getMilestonePopup()
-  document.getElementById("popup").style.display = (isShowingMilestone)? 'block' : 'none'
+  document.getElementById("popup").style.display = (isShowingMilestone) ? 'block' : 'none'
   if (time < totalTime) {
-    document.getElementById("countdown").innerHTML = "距离当前结局还有" + formatTime(totalTime - time)
+    document.getElementById("countdown").innerHTML = "模拟已运行" + formatTime(time)
   }
   if (time >= totalTime && !endScreenShowed) {
-    document.getElementById("countdown").innerHTML = "距离当前结局还有0分钟 0.00秒"
+    document.getElementById("countdown").innerHTML = "模拟已运行"+formatTime(time)
     endScreenShowed = true
     document.getElementById("countdown").classList.add('hide');
     document.getElementById("milestones").classList.add('hide');
-    setTimeout(function(){
-      document.getElementById("countdown").innerHTML = "已到达当前结局"
+    setTimeout(function() {
+      setInterval(function(){
+        document.getElementById("countdown").innerHTML = "已达结局："+formatTime(time)
+      })
       document.getElementById("countdown").classList.remove('hide');
-    },1500)
+    }, 1500)
   }
 }
 
@@ -58,66 +63,100 @@ function timeToNumber(x) {
   //10^^^4 < x < 10^^^1e6
   else if (x < 89348) {
     return "10^^^" + Number((1.0015 ** (x - 80131) + 0.00008 * x - 3.41).toFixed(2)).toLocaleString()
-  //10^^^1e6 < x
+    //10^^^1e6 < x
   } else if (x < 130069) {
     return "10^^^" + timeToNumber((x - 86550) ** 1.1)
   } else if (x < 132569) {
-    return "10^^^^" + Number(((( x - 130069) / 500)  + 5).toFixed(2)).toLocaleString()
+    return "10^^^^" + Number((((x - 130069) / 500) + 5).toFixed(2)).toLocaleString()
   } else if (x < 185817) {
     a = ((1.01 ** ((x - 116394) / 100)) ** 2) - 20
     c = Math.floor(a)
     b = (10 ** ((a - c) * (1 - Math.log10(2)) + Math.log10(2))).toFixed(2)
-    if (c<10) return 10 + '^'.repeat(c) + b
+    if (c < 10) return 10 + '^'.repeat(c) + b
     return `10{${c.toLocaleString()}}${b}`
   } else if (x < 229228) {
     return '10{' + timeToNumber((x - 183836) ** 1.15) + '}10'
   } else if (x < 233834) {
-    return "10{{1}}"+Number((1.0015 ** (x - 229228) + 0.00008 * x - 14.34).toFixed(2)).toLocaleString()
+    return "10{{1}}" + Number((1.0015 ** (x - 229228) + 0.00008 * x - 14.34).toFixed(2)).toLocaleString()
   } else if (x < 288198) {
     return '10{{1}}' + timeToNumber((x - 232515) ** 1.15)
+  } else if (x < 1033200) {
+    return '10{{2}}' + timeToNumber((x - 287800) * 1.2)
+  } else if (x < 1233000) {
+    return '10{{3}}'+Number((((x - 1033200) / 50000) + 6).toFixed(2)).toLocaleString()
+  } else if (x < 1857554) {
+    a = ((1.01 ** ((x - 1163330) / 1000)) ** 2)
+    c = Math.floor(a)
+    b = (10 ** ((a - c) * (1 - Math.log10(2)) + Math.log10(2))).toFixed(2)
+    return `10{{${c.toLocaleString()}}}${b}`
   } else {
-    return '10{{2}}5'
+    return '10{{1,000,000}}10'
   }
 }
-
 setInterval(update, 10)
 
-function formatTime(t) {
-  return Math.floor(t / 6000) + "分钟 " + ((t / 100) % 60).toFixed(2) + "秒"
+function formatTime(t) {  
+  const totalSeconds = Math.floor(t / 100); // 总秒数  
+  const days = Math.floor(totalSeconds / (24 * 60 * 60)); // 天数  
+  const hours = Math.floor((totalSeconds % (24 * 60 * 60)) / (60 * 60)); // 小时数  
+  const minutes = Math.floor((totalSeconds % (60 * 60)) / 60); // 分钟数  
+  const seconds = (totalSeconds % 60).toFixed(0); // 秒数，保留两位小数  
+  
+  let formattedTime = "";  
+  
+  if (days > 0) {  
+    formattedTime += days + "天 ";  
+  }  
+  
+  if (hours > 0) {  
+    formattedTime += hours + "小时 ";  
+  }  
+  
+  if (minutes > 0) { // 如果已经有天数或小时数，或者分钟数大于0，则显示分钟  
+    formattedTime += minutes + "分钟 ";  
+  }  
+  
+  formattedTime += seconds + "秒";  
+  
+  return formattedTime;  
 }
 
-  
-function getMilestoneText() {  
+function getMilestoneText() {
   // 找到大于time的最小里程碑索引  
-  var index = milestones.findIndex(function(milestone) {  
-    return milestone > time;  
-  });  
-  
+  var index = milestones.findIndex(function(milestone) {
+    return milestone > time;
+  });
   // 如果没有找到大于time的里程碑（即index为-1）  
-  if (index === -1) {  
+  if (index === -1) {
     // 返回已达到所有里程碑的信息  
-    return '已达到所有里程碑';  
-  } else {  
+    return '已达到所有里程碑';
+  } else {
     // 返回最近的里程碑信息  
     percent = ' (' + (((time - milestones[index - 1]) / (milestones[index] - milestones[index - 1]) * 100).toFixed(2) + '%') + ')'
-    return '最近的里程碑：' + milestoneDisplays[index] + percent;  
-  }  
-}  
+    return '最近的里程碑：' + milestoneDisplays[index] + percent;
+  }
+}
+
 function getMilestonePopup() {
   mst = ''
-  for (var i = 0; i < milestones.length; i++) {  
-    // 计算完成百分比，确保至少为1%  
-    var percentage = format((time / milestones[i]) * 100);  
-    
-    // 显示里程碑和完成百分比  
-    mst+=('里程碑' + (i + 1) + '：要求：' + milestoneDisplays[i] + ' 已完成' + percentage + '%' + '<br>');  
+  for(var i = 0; i < milestones.length; i++) {
+    var percentage = format((time / milestones[i]) * 100);
+    mst += ('里程碑' + (i + 1) + '：要求：' + milestoneDisplays[i] + ' 已完成' + percentage + '%' + '<br>');
   }
-  mst+=`<br><button class='btn' onclick="isShowingMilestone = false">关闭</btn>`
+  mst += `<br><button class='btn' onclick="isShowingMilestone = false">关闭</btn>`
   return mst
 }
+
 function format(num) {
-  if (Number.isFinite(num)) return num.toFixed(2)
-  else if (Math.sign(num) == -1) return '-<div class="rotate-90">8</div>'
-  else return '+<div class="rotate-90">8</div>'
+  if (Number.isFinite(num)) {
+    if (num < 1e3) return num.toFixed(2)
+    else {
+      exponent = Math.floor(Math.log10(num))
+      mantissa = num / (10 ** exponent)
+      return mantissa.toFixed(2) + 'e' + exponent
+    }
+  }
+  else if (Math.sign(num) == -1) return '-' + Infinity_symbol
+  else return '+' + Infinity_symbol
 }
 console.log('总计时间：' + formatTime(totalTime))
